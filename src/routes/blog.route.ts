48fe7ccfa81cadaf359c8f2ec8d38fdc9becfff6
sherif-abdalhakam <sherif.abdalhakam@vodafone.com>
2025-07-29 01:23:33 +0300

    Implement blog creation

--- src/routes/blog.route.ts
+++ src/routes/blog.route.ts
@@ -1,5 +1,8 @@
 import { Router } from "express";
 import { asyncRoute } from "@/utils/async-route.util";
+import { validateBlog } from "@/models/blog";
+import { RESPONSE_STATUS, VALIDATION_MESSAGES } from "@/constants";
+import Blog from "@/models/blog/blog.model";
 
 const router = Router();
 
@@ -12,7 +15,25 @@ router.get("/:id", asyncRoute(async (req, res) => {
 }));
 
 router.post("/", asyncRoute(async (req, res) => {
-  res.send("Create Blog...");
+  const { error, success, data } = validateBlog(req.body);
+
+  if (!success) {
+    return res.status(RESPONSE_STATUS.BAD_REQUEST).json({
+      errors: error,
+    });
+  }
+
+  const isBlogExists = await Blog.findOne({ title: data?.title });
+
+  if (isBlogExists) {
+    return res.status(RESPONSE_STATUS.BAD_REQUEST).json({
+      message: VALIDATION_MESSAGES.ITEM_ALREADY_EXISTS,
+    });
+  }
+
+  const blog = await Blog.create(data);
+
+  res.status(RESPONSE_STATUS.CREATED).json(blog);
 }));
 
 router.put("/:id", asyncRoute(async (req, res) => {
