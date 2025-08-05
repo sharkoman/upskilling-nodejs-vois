e2e8d0c2f3b7b7dd3f287f620558a07e1bd2f462
sherif-abdalhakam <sherif.abdalhakam@vodafone.com>
2025-08-05 17:25:23 +0300

    Update Swagger configuration to enhance API documentation

--- src/docs/swagger.config.ts
+++ src/docs/swagger.config.ts
@@ -3,13 +3,12 @@ import swaggerDocument from './swagger.json';
 export const swaggerSpec = swaggerDocument;
 
 export const swaggerUiOptions = {
-  customCss: '.swagger-ui .topbar { display: none }',
   customSiteTitle: "Node Upskilling API Documentation",
   swaggerOptions: {
     persistAuthorization: true,
     displayRequestDuration: true,
     docExpansion: 'none',
-    filter: true,
+    filter: false,
     showExtensions: true,
     showCommonExtensions: true,
     tryItOutEnabled: true
